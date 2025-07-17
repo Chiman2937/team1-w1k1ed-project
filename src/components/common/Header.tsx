c432526c7026ca506cfb3ccd0bbec3eff8f4be10
Bae Min Ji <vvqoalswlvv@gmail.com>
2025-07-17 14:06:22 +0900

    feat: Header 컴포넌트 UI구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,7 @@
-import LogoImage from '@/assets/images/logo.svg'; // 로컬 이미지 파일 임포트
+import LogoImage from '@/assets/images/logo.svg';
 import Link from 'next/link';
 import Nav from './Nav';
+import Menu from '@/assets/images/type=menu.svg';
 
 export default function Header() {
   return (
@@ -10,12 +11,20 @@ export default function Header() {
           <LogoImage alt='로고' width={107} height={30} className='h-8 w-auto' />
         </Link>
 
-        <Nav />
+        <div className='hidden md:block'>
+          <Nav />
+        </div>
       </div>
-      <Link href='/login'>
-        {' '}
-        <button className='text-gray-400 cursor-pointer'>로그인</button>
-      </Link>
+
+      <div className='flex items-center gap-4'>
+        <Link href='/login'>
+          <button className='hidden md:inline text-gray-400 cursor-pointer'>로그인</button>
+        </Link>
+
+        <button className='inline md:hidden'>
+          <Menu className='w-6 h-6 text-gray-600' /> {/* 햄버거 아이콘 */}
+        </button>
+      </div>
     </div>
   );
 }
