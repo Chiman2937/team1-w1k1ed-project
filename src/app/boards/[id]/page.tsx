489d15f780a95d97c79a0a19a773f95bf315d816
3rdflr <3rdflrhtl@gmail.com>
2025-07-18 21:36:40 +0900

    solveing many problems

--- src/app/boards/[id]/page.tsx
+++ src/app/boards/[id]/page.tsx
@@ -2,13 +2,8 @@ import BoardComments from '@/components/page/boardDetail/BoardComments';
 import BoardContent from '@/components/page/boardDetail/BoardContent';
 import BoardDetail from '@/components/page/boardDetail/BoardDetail';
 
-interface IParams {
-  params: {
-    id: string;
-  };
-}
-
-export default function Board({ params: { id } }: IParams) {
+export default async function Board({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
   return (
     <>
       <div className='flex flex-col items-center justify-center'>
