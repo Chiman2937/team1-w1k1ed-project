a64b9c4321c54ccc349d5cfdede08c3d49bb00ea
Chiman2937 <cyga2933@gmail.com>
2025-07-21 17:01:02 +0900

    test: testeditor page.tsx 스타일 임시 변경

--- src/app/testeditor/page.tsx
+++ src/app/testeditor/page.tsx
@@ -4,7 +4,7 @@ import TextEditor from './TextEditor';
 
 const TestEditor = () => {
   return (
-    <div className='p-3'>
+    <div className='p-3 max-w-[960px] mx-auto'>
       <TextEditor />
     </div>
   );
