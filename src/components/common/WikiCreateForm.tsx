5a434360d7a1296e2db3e804dc4c1a8ad2bdd1e6
dain <gunmannduu@gmail.com>
2025-07-21 15:57:20 +0900

    빌드에러 수정

--- src/components/common/WikiCreateForm.tsx
+++ src/components/common/WikiCreateForm.tsx
@@ -3,25 +3,21 @@
 import Input from '@/components/common/Input';
 import Button from '@/components/common/Button';
 
-interface Props {
-  isMdUp: boolean;
-}
-
-const WikiCreateForm = ({ isMdUp }: Props) => {
+const WikiCreateForm = () => {
   return (
     <form className='flex flex-col gap-[8px] mx-auto'>
       <Input
+        className='w-[335px] md:w-[400px]'
         label='위키 생성하기'
         type='text'
         placeholder='질문을 입력해 주세요'
         name='question'
-        variant={isMdUp ? 'L' : 'S'}
       />
       <Input
+        className='w-[335px] md:w-[400px]'
         type='text'
         placeholder='답을 입력해 주세요'
         name='answer'
-        variant={isMdUp ? 'L' : 'S'}
       />
       <div className='flex justify-end mt-[16px]'>
         <Button type='submit'>생성하기</Button>
