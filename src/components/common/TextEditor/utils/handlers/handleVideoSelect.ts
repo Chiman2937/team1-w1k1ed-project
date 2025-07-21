fb249e3a9488eab8e128632b98f7b4f15603ac25
Chiman2937 <cyga2933@gmail.com>
2025-07-22 01:02:45 +0900

    refactor: toolbar 이미지, 동영상, 링크 이벤트 구문 별도 파일로 분리

---
+++ src/components/common/TextEditor/utils/handlers/handleVideoSelect.ts
@@ -0,0 +1,27 @@
+import { Editor } from '@tiptap/react';
+
+export const handleVideoSelect = (
+  e: React.ChangeEvent<HTMLInputElement>,
+  editor: Editor | null,
+  setVideos: React.Dispatch<React.SetStateAction<Record<string, File>>>,
+) => {
+  const file = e.target.files?.[0];
+  if (!file || !editor) return;
+
+  const videoBlobURL = URL.createObjectURL(file);
+
+  setVideos((prev) => ({
+    ...prev,
+    [videoBlobURL]: file,
+  }));
+
+  editor
+    .chain()
+    .focus()
+    .insertLocalVideo({
+      src: videoBlobURL,
+      width: '640',
+      height: '360',
+    })
+    .run();
+};
