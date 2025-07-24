98f383d81a98be44027ef8adee9bbf95e5ac3a53
Chiman2937 <cyga2933@gmail.com>
2025-07-24 14:46:27 +0900

    refactor: testeditor page 구조 수정

--- src/app/testeditor/page.tsx
+++ src/app/testeditor/page.tsx
@@ -2,18 +2,29 @@
 
 import { useTextEditor } from '@/components/common/TextEditor/utils/hooks/useTextEditor';
 import ToolBar from '../../components/common/TextEditor/ToolBar';
-import { EditorContent } from '@tiptap/react';
+import { handlehtmlParse } from '@/components/common/TextEditor/utils/handlers/handleHtmlParse';
+import { useState } from 'react';
+import ContentViewer from '@/components/common/TextEditor/ContentViewer';
+import ContentEditor from '@/components/common/TextEditor/ContentEditor';
 
 const TestEditor = () => {
-  const { editor } = useTextEditor();
+  const { editor, tempFiles, setTempFiles } = useTextEditor();
+
+  const [content, setContent] = useState('');
+
+  const handleRenderClick = async () => {
+    if (!editor) return;
+    const nextContent = await handlehtmlParse({ editor, files: tempFiles });
+    setContent(nextContent);
+  };
+
   if (!editor) return;
   return (
     <div className='p-3 max-w-[960px] mx-auto flex flex-col gap-[20px]'>
-      <ToolBar editor={editor} />
-      {/* Text Area 컴포넌트 */}
-      <div className='px-5'>
-        <EditorContent editor={editor} />
-      </div>
+      <ToolBar editor={editor} setTempFiles={setTempFiles} />
+      <ContentEditor editor={editor} />
+      <button onClick={handleRenderClick}>파싱 시작</button>
+      <ContentViewer content={content} />
     </div>
   );
 };
