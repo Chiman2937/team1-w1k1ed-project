53d48330f0ee323089e9700348902868ab75c37a
Bae Min Ji <vvqoalswlvv@gmail.com>
2025-07-26 01:29:13 +0900

    fix: details

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,8 +3,9 @@ const Footer = () => {
     <div className='w-full text-center mx-auto bg-grayscale-100 text-grayscale-500 overflow-hidden'>
       <footer
         className='text-left bg-grayscale-600 text-white font-pretendard
-      px-[20px] py-[40px]
-      md:py-[60px]
+        px-[20px] py-[40px]
+        md:px-[40px] md:py-[60px]
+        lg:px-[80px]
       '
       >
         <div
@@ -20,16 +21,16 @@ const Footer = () => {
           </p>
           <div
             className='
-          text-[8px] mt-[10px] mb-[20px]
-          md:text-[14px] md:mb-[30px]'
+            text-[8px] mt-[10px] mb-[20px]
+            md:text-[14px] md:mb-[30px]'
           >
             <p>사업자등록번호 000-00-00000 | 통신판매신고 제2020-서울-00000호 | 대표 : 이지은</p>
             <p>서울특별시 중구 청계천로 123, 위키드빌딩</p>
           </div>
           <div
             className='flex gap-[30px] 
-          text-[8px] my-[20px]
-          md:text-[14px] 
+            text-[8px] my-[20px]
+            md:text-[14px] 
           '
           >
             <p>서비스 이용 약관</p>
