9f04dd856e5844f2b20443018254c6b238de465d
Chiman2937 <cyga2933@gmail.com>
2025-07-26 14:56:58 +0900

    fix: timer 완료 시 ontimerFinish 실행 될 수 있도록 props 추가

--- src/components/page/wikicode/Timer/Timer.tsx
+++ src/components/page/wikicode/Timer/Timer.tsx
@@ -1,15 +1,16 @@
-import { getMinutesFromSeconds } from '@/utils/time';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 interface Props {
   registeredAt: string | undefined;
+  onTimerFinish: () => void;
 }
 
-const Max_Timer = 10;
+const Max_Timer = 300;
 
-const Timer = ({ registeredAt }: Props) => {
+const Timer = ({ registeredAt, onTimerFinish }: Props) => {
   // 남은 수정시간 타이머
   const [timer, setTimer] = useState<number | null>(null);
+  // setInterval 절대 참조 Ref
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   // 남은 수정시간 계산
@@ -21,24 +22,34 @@ const Timer = ({ registeredAt }: Props) => {
   };
 
   // 남은 수정시간 카운트다운
-  const runCountdown = (): NodeJS.Timeout => {
+  const runCountdown = useCallback((): NodeJS.Timeout => {
     return setInterval(() => {
       setTimer((prev) => {
         if (prev === null) return null;
         return prev - 1;
       });
     }, 1000);
+  }, []);
+
+  // 숫자를 `m분 s초` 로 formatting
+  const formatMinutesFromSeconds = (totalSeconds: number) => {
+    const minute = ~~(totalSeconds / 60);
+    const second = totalSeconds % 60;
+    return `${minute}분 ${second}초`;
   };
 
-  // timer가 0이 되면 TimerIntervalRef 초기화, isTimerFinish true 상태로 전환
+  // timer가 0이 되면
+  // timer null로 전환
+  // TimerIntervalRef 초기화 상태로 전환
+  // onTimerFinish 실행
   useEffect(() => {
-    console.log(`timer 변화 감지: ${timer}`);
     if (!intervalRef.current) return;
     if (timer === null) return;
     if (timer > 0) return;
     clearInterval(intervalRef.current);
     setTimer(null);
-  }, [timer]);
+    onTimerFinish();
+  }, [timer, onTimerFinish]);
 
   // 수정 상태가 등록되어 registeredAt이 falsy 값이 아니면 timerInterval 실행
   useEffect(() => {
@@ -51,14 +62,13 @@ const Timer = ({ registeredAt }: Props) => {
     return () => {
       if (intervalRef.current) {
         clearInterval(intervalRef.current);
-        setTimer(null);
       }
     };
-  }, [registeredAt]);
+  }, [registeredAt, runCountdown]);
 
   if (!registeredAt === null || timer === null) return;
 
-  return <div>남은시간: {getMinutesFromSeconds(timer)}</div>;
+  return <div>남은시간: {formatMinutesFromSeconds(timer)}</div>;
 };
 
 export default Timer;
