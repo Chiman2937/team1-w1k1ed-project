f66ecba942503619e41ae40d37ab197a758c9d02
Jinsun Kim <jinsunkim94@gmail.com>
2025-07-28 20:53:39 +0900

    feat: 게시판 등록에 로그인 안내 스낵바 추가

--- src/components/page/boards/BoardsHeader.tsx
+++ src/components/page/boards/BoardsHeader.tsx
@@ -1,12 +1,56 @@
-import Button from '@/components/common/Button';
+'use client';
 
-const BoardsHeader = () => {
+import Button from '@/components/common/Button';
+import SnackBar from '@/components/common/Snackbar';
+import { useAuthContext } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+export default function BoardsHeader() {
+  const { isAuthenticated } = useAuthContext();
+  const router = useRouter();
+
+  const [showToast, setShowToast] = useState(false);
+  const [isOpening, setIsOpening] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleClick = () => {
+    if (isAuthenticated) {
+      router.push('/addboard');
+    } else {
+      setShowToast(true);
+      setIsOpening(true);
+
+      // 닫힘 애니메이션 타이밍
+      setTimeout(() => {
+        setIsOpening(false);
+        setIsClosing(true);
+      }, 1000);
+
+      setTimeout(() => {
+        setShowToast(false);
+        setIsClosing(false);
+      }, 1000);
+
+      // 로그인 화면 전환을 약간 늦춤
+      setTimeout(() => {
+        router.push('/login');
+      }, 1000);
+    }
+  };
+
   return (
-    <section className='w-full flex justify-between py-10 items-center'>
+    <section className='w-full flex justify-between py-10 items-center relative'>
       <h1 className='text-grayscale-500 text-2xl-semibold'>베스트 게시글</h1>
-      <Button href='/addboard'>게시물 등록하기</Button>
+      <Button onClick={handleClick} className='btn'>
+        게시물 등록하기
+      </Button>
+
+      {showToast && (
+        <SnackBar variant='info' isOpening={isOpening} isClosing={isClosing} index={0}>
+          로그인이 필요합니다.
+        </SnackBar>
+      )}
     </section>
   );
-};
-
-export default BoardsHeader;
+}
