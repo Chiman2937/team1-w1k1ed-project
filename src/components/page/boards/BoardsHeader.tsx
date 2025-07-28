3a9eecbb5fc70b6b3c20a61183e77ffb83736410
Jinsun Kim <jinsunkim94@gmail.com>
2025-07-28 20:24:49 +0900

    fix: 게시판 리스트 API정리 및 등록하기 버튼에 URL추가

--- src/components/page/boards/BoardsHeader.tsx
+++ src/components/page/boards/BoardsHeader.tsx
@@ -4,7 +4,7 @@ const BoardsHeader = () => {
   return (
     <section className='w-full flex justify-between py-10 items-center'>
       <h1 className='text-grayscale-500 text-2xl-semibold'>베스트 게시글</h1>
-      <Button>게시물 등록하기</Button>
+      <Button href='/addboard'>게시물 등록하기</Button>
     </section>
   );
 };
