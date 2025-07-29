875b5b3e29b890f601c07b8c92f9e329d3ac6cce
Jinsun Kim <jinsunkim94@gmail.com>
2025-07-29 14:27:32 +0900

    fix:스낵바를 토스트로 변경

--- src/components/page/boards/BoardsHeader.tsx
+++ src/components/page/boards/BoardsHeader.tsx
@@ -4,33 +4,21 @@ import Button from '@/components/common/Button';
 import SnackBar from '@/components/common/Snackbar';
 import { useAuthContext } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { toast } from 'cy-toast';
 
 export default function BoardsHeader() {
   const { isAuthenticated } = useAuthContext();
   const router = useRouter();
 
-  const [showToast, setShowToast] = useState(false);
-  const [isOpening, setIsOpening] = useState(false);
-  const [isClosing, setIsClosing] = useState(false);
-
   const handleClick = () => {
     if (isAuthenticated) {
       router.push('/addboard');
     } else {
-      setShowToast(true);
-      setIsOpening(true);
-
-      // 닫힘 애니메이션 타이밍
-      setTimeout(() => {
-        setIsOpening(false);
-        setIsClosing(true);
-      }, 1000);
-
-      // 로그인 화면 전환을 약간 늦춤
-      setTimeout(() => {
-        router.push('/login');
-      }, 1000);
+      toast.run(({ isClosing, isOpening, index }) => (
+        <SnackBar variant='info' isOpening={isOpening} isClosing={isClosing} index={index}>
+          로그인이 필요한 서비스입니다.
+        </SnackBar>
+      ));
     }
   };
 
@@ -40,12 +28,6 @@ export default function BoardsHeader() {
       <Button onClick={handleClick} className='btn'>
         게시물 등록하기
       </Button>
-
-      {showToast && (
-        <SnackBar variant='info' isOpening={isOpening} isClosing={isClosing} index={0}>
-          로그인이 필요한 서비스입니다.
-        </SnackBar>
-      )}
     </section>
   );
 }
