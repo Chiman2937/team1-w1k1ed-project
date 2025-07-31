378fc058491974e0c17a17d9b8940d72a636ba13
Chiman2937 <cyga2933@gmail.com>
2025-07-31 11:58:10 +0900

    feat: 위키 상세페이지 content가 비어있으면 대신 보여줄 UI 제작

---
+++ src/components/page/wikicode/ProfileNoContent/ProfileNoContent.tsx
@@ -0,0 +1,52 @@
+import Button from '@/components/common/Button';
+import { getHtmlStringIsEmpty } from '@/components/common/TextEditor/utils/handlers/getHtmlStringIsEmpty';
+import { useAuthContext } from '@/context/AuthContext';
+import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
+
+interface Props {
+  content: string;
+}
+
+const ProfileNoContent = ({ content }: Props) => {
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  const isEmpty = getHtmlStringIsEmpty(content);
+  const isLogin = user !== null;
+
+  const handleLoginClick = () => {
+    router.push('/login');
+  };
+
+  if (!isEmpty) return null;
+
+  return (
+    <div className={clsx('bg-gray-100 rounded-[15px] py-[40px]', 'flex flex-col items-center')}>
+      {isLogin && (
+        <div className='flex flex-col items-center gap-[10px]'>
+          <p className='text-center text-grayscale-400 text-lg-regular'>
+            아직 작성된 내용이 없네요.
+            <br />
+            위키에 참여해 친구의 정보를 작성해보세요!
+          </p>
+          <Button variant='primary'>위키 참여하기</Button>
+        </div>
+      )}
+      {!isLogin && (
+        <div className='flex flex-col items-center gap-[10px]'>
+          <p className='text-center text-grayscale-400 text-lg-regular'>
+            아직 작성된 내용이 없네요.
+            <br />
+            로그인 후 위키에 참여해보세요!
+          </p>
+          <Button variant='primary' onClick={handleLoginClick}>
+            로그인
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfileNoContent;
